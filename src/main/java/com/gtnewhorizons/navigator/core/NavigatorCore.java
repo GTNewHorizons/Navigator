406db968344360ab9d63fef7bc37239921946440
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-07-14 17:46:39 +0200

    Convert to new IMixins and MixinsBuilder system (#12)

--- src/main/java/com/gtnewhorizons/navigator/core/NavigatorCore.java
+++ src/main/java/com/gtnewhorizons/navigator/core/NavigatorCore.java
@@ -7,12 +7,14 @@ import java.util.Set;
 import com.gtnewhorizon.gtnhlib.config.ConfigException;
 import com.gtnewhorizon.gtnhlib.config.ConfigurationManager;
 import com.gtnewhorizon.gtnhmixins.IEarlyMixinLoader;
+import com.gtnewhorizon.gtnhmixins.builders.IMixins;
 import com.gtnewhorizons.navigator.config.GeneralConfig;
 import com.gtnewhorizons.navigator.config.ModuleConfig;
 import com.gtnewhorizons.navigator.mixins.Mixins;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
+@IFMLLoadingPlugin.MCVersion("1.7.10")
 public class NavigatorCore implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
     static {
@@ -54,6 +56,6 @@ public class NavigatorCore implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
     @Override
     public List<String> getMixins(Set<String> loadedCoreMods) {
-        return Mixins.getEarlyMixins(loadedCoreMods);
+        return IMixins.getEarlyMixins(Mixins.class, loadedCoreMods);
     }
 }
