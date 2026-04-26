403de94fcc76762a64516e1581692ff24b62bc3b
Eldrinn-Elantey <46845681+Eldrinn-Elantey@users.noreply.github.com>
2026-04-26 18:35:23 +0400

    Update RenderWaypointBeaconMixin.java

--- src/main/java/com/gtnewhorizons/navigator/mixins/late/journeymap/RenderWaypointBeaconMixin.java
+++ src/main/java/com/gtnewhorizons/navigator/mixins/late/journeymap/RenderWaypointBeaconMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.gtnewhorizons.navigator.api.NavigatorApi;
@@ -18,15 +19,26 @@ import com.gtnewhorizons.navigator.api.model.waypoints.WaypointManager;
 import journeymap.client.model.Waypoint;
 import journeymap.client.render.ingame.RenderWaypointBeacon;
 
-@Mixin(value = RenderWaypointBeacon.class, remap = false)
+@Mixin(RenderWaypointBeacon.class)
 public abstract class RenderWaypointBeaconMixin {
 
     @Shadow(remap = false)
     static Minecraft mc;
 
     @Shadow(remap = false)
-    static void doRenderNoFade(Waypoint waypoint) {
-        throw new IllegalStateException("Mixin failed to shadow doRenderNoFade()");
+    static void doRender(Waypoint waypoint) {
+        throw new IllegalStateException("Mixin failed to shadow doRender()");
+    }
+
+    private static boolean skipFade = false;
+
+    @ModifyVariable(
+        method = "doRender(Ljourneymap/client/model/Waypoint;F)V",
+        at = @At(value = "STORE"),
+        name = "fadeAlpha",
+        remap = false)
+    private static float overrideFadeAlpha(float fadeAlpha) {
+        return skipFade ? 1.0F : fadeAlpha;
     }
 
     @Inject(
@@ -37,15 +49,20 @@ public abstract class RenderWaypointBeaconMixin {
         remap = false,
         require = 1)
     private static void navigator$onRenderAll(float partialTicks, CallbackInfo ci) {
-        for (InteractableLayerManager layer : NavigatorApi.getInteractableLayers()) {
-            WaypointManager waypointManager = layer.getWaypointManager(JourneyMap);
-            if (waypointManager instanceof JMWaypointManager jmWaypointManager && waypointManager.hasWaypoint()) {
-                final Waypoint waypoint = jmWaypointManager.getJmWaypoint();
-                if (waypoint.getDimensions()
-                    .contains(mc.thePlayer.dimension)) {
-                    doRenderNoFade(waypoint);
+        skipFade = true;
+        try {
+            for (InteractableLayerManager layer : NavigatorApi.getInteractableLayers()) {
+                WaypointManager waypointManager = layer.getWaypointManager(JourneyMap);
+                if (waypointManager instanceof JMWaypointManager jmWaypointManager && waypointManager.hasWaypoint()) {
+                    final Waypoint waypoint = jmWaypointManager.getJmWaypoint();
+                    if (waypoint.getDimensions()
+                        .contains(mc.thePlayer.dimension)) {
+                        doRender(waypoint);
+                    }
                 }
             }
+        } finally {
+            skipFade = false;
         }
     }
 }
